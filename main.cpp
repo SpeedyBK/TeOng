2d63b1d4073590d11b26dbe09eb88e3a4151ec11
Benjamin Lagershausen-Kessler <benjamin.kessler01@googlemail.com>
2023-09-26 23:58:32 +0200

    2023

--- main.cpp
+++ main.cpp
@@ -1,15 +1,20 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <cmath>
 #include "Pong/Pong.h"
 #include "Tetris/Tetris.h"
 #include "chess/chess.h"
 
 void GameSelect(int selector);
+void printPiInBinary(float* pi);
 
 int main()
 {
     using namespace std;
     int selector = 0;
+
+    auto pi = (float) M_PI;
+    printPiInBinary(&pi);
 
     std::cout << "Game Menu: " << std::endl;
     std::cout << "Type 1 for Pong, type 2 for Tetris or type 3 for Chess" << std::endl;
@@ -25,7 +30,6 @@ void GameSelect(int selector){
 
     std::cout << selector << std::endl;
 
-
     if (selector == 1) {
         std::cout << "Running Pong!" << std::endl;
         Pong pong;
@@ -40,5 +44,20 @@ void GameSelect(int selector){
         std::cin >> selector;
         GameSelect(selector);
     }
+}
 
+void printPiInBinary(float* pi){
+    auto lptr = (int*) pi;
+    std::array<int, 32> pi_bin = {};
+
+    for (int i = pi_bin.size()-1; i >= 0; i--){
+        pi_bin.at(i) = *lptr & 1;
+        *lptr = *lptr >> 1;
+    }
+
+    std::cout << "This is Pi: " << std::endl;
+    for (auto &it : pi_bin){
+        std::cout << it;
+    }
+    std::cout << std::endl;
 }
